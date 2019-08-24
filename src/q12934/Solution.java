b35316dfd4a9b7e9af164d091a63353428eda5ff
chaibin0 <kukulza0@naver.com>
2019-08-25 00:08:40 +0900

    2019-08-25

--- src/q12934/Solution.java
+++ src/q12934/Solution.java
@@ -4,7 +4,6 @@ class Solution {
 	public long solution(long n) {
 		long answer=0;
 		double sqrt = Math.sqrt(n);
-		System.out.println(sqrt);
 		if (sqrt - (long) sqrt != 0.0) {
 			return -1;
 		}
--- src/q12944/Solution.java
+++ src/q12944/Solution.java
@@ -1,10 +1,10 @@
 package q12944;
 
 class Solution {
-	  public double solution(int[] arr) {
-		  int sum=0;
-		  for(int element : arr)
-			  sum+=element;
-	      return (double)sum/arr.length;
-	  }
+	public double solution(int[] arr) {
+		int sum = 0;
+		for (int element : arr)
+			sum += element;
+		return (double) sum / arr.length;
 	}
+}
